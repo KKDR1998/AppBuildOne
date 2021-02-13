7f5cfd03ba53fb90945fd1a8f8195973ad502c34
KKDR1998 <77356024+KKDR1998@users.noreply.github.com>
2021-02-12 14:04:01 -1000

    AppBuildOne - first view controller completed

--- AppBuildOne/ViewController.swift
+++ AppBuildOne/ViewController.swift
@@ -9,11 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var uhwoInfo: UILabel!
+    
+    
 // Hello
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        uhwoInfo.text = "The new Academy for Creative Media West O'ahu facility is a place where students can grow."
+        
     }
+    
+    
+    
+    
+    @IBAction func ACM(_ sender: Any)
+    {
+        uhwoInfo.text = "UH West O'ahu's Creative Media program embraces digital media literacy experienced through vidoe, animation, video games, design, social media, web and app development, virtual and augmented reality, other forms of media communication and design and digital storytelling."
+    }
+    
+    
+    @IBAction func UHWO(_ sender: Any)
+    {
+        uhwoInfo.text = "The new Academy for Creative Media West O'ahu facility is a place where students can grow."
+        
+        
+    }
+    
+    
+    
 
 
 }
